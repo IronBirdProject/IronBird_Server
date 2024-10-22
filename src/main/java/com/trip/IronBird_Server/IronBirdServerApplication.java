fb7ff5114ebbdc3fecdd45e4e2449d8d08d6f074
Haejeong079 <goesed7582@naver.com>
2024-10-22 15:06:00 +0900

    first project commit

---
+++ src/main/java/com/trip/IronBird_Server/IronBirdServerApplication.java
@@ -0,0 +1,13 @@
+package com.trip.IronBird_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronBirdServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronBirdServerApplication.class, args);
+	}
+
+}
