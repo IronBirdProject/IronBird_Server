117fe6be4b02b22ff8ed17adb32a6c6b906367d3
Haejeong079 <goesed7582@naver.com>
2025-01-07 14:53:34 +0900

    [FEATURE:PLAN]

--- src/main/java/com/trip/IronBird_Server/plan/repository/PlanRepository.java
+++ src/main/java/com/trip/IronBird_Server/plan/repository/PlanRepository.java
@@ -4,6 +4,10 @@ import com.trip.IronBird_Server.plan.domain.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlanRepository extends JpaRepository<Plan,Long> {
+    List<Plan> findByUserId(Long userId);
+
 }
