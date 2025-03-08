d8aa2701435bd95619aba3bfcdea3a6d83f8468b
Haejeong079 <goesed7582@naver.com>
2025-03-08 23:53:40 +0900

    :bug: uploadImages

--- src/main/java/com/trip/IronBird_Server/post/adapter/controller/PostController.java
+++ src/main/java/com/trip/IronBird_Server/post/adapter/controller/PostController.java
@@ -4,12 +4,14 @@ import com.trip.IronBird_Server.common.custom.CustomUserDetails;
 import com.trip.IronBird_Server.post.adapter.dto.PostDto;
 import com.trip.IronBird_Server.post.adapter.dto.UploadImageDto;
 import com.trip.IronBird_Server.post.application.service.PostServiceImp;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -34,18 +36,22 @@ public class PostController {
     /**
      * 포스팅 생성 컨트롤러
      */
-    @PostMapping(value = "/create")
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto,
-                                              @ModelAttribute UploadImageDto uploadImageDto,
+    @PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<PostDto> createPost(@RequestPart PostDto postDto,
+                                              @RequestPart List<MultipartFile> images,
                                               @AuthenticationPrincipal CustomUserDetails userDetails) {
         if (userDetails == null) {
             throw new IllegalArgumentException("인증 정보가 없습니다.");
         }
         String email = userDetails.getEmail();  // 이메일 가져오기
-        PostDto createdPost = postServiceImp.createPost(postDto, uploadImageDto, email);
 
+        UploadImageDto uploadImageDto = new UploadImageDto();
+        uploadImageDto.setImages(images);
+
+        PostDto createdPost = postServiceImp.createPost(postDto, uploadImageDto, email);
         return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
     }
+
 
 
 
