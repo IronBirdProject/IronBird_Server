81784253f7cc563d13ad83ab525976eb927a06d2
Haejeong079 <goesed7582@naver.com>
2025-06-05 00:44:34 +0900

    UserInfo Token

--- src/main/java/com/trip/IronBird_Server/jwt/dto/TokenDto.java
+++ src/main/java/com/trip/IronBird_Server/jwt/dto/TokenDto.java
@@ -1,5 +1,7 @@
 package com.trip.IronBird_Server.jwt.dto;
 
+import com.trip.IronBird_Server.user.domain.entity.User;
+import com.trip.IronBird_Server.user.domain.modeltype.OauthType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +17,26 @@ public class TokenDto {
     private final Long accessTokenExpiresIn;
     private final Long refreshTokenExpiresIn;
     private final String refreshToken;
+
+    private final UserInfo user;    // 사용자 정보 포함
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class UserInfo{
+        private String email;
+        private String name;
+        private String oauthId;
+        private String profilePic;
+
+        public UserInfo(Long id, String email, String name, OauthType oauthType, String profilePic) {
+            this.email = email;
+            this.name = name;
+            this.oauthId = oauthType != null ? oauthType.name() : null;
+            this.profilePic = profilePic;
+        }
+
+    }
 
 
     @Data
